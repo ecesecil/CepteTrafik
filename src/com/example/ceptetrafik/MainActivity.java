b2d81d6e9db1a39a54fa962630cd8a0bdfd2ca4e
Ece Seçil Akbaş <esecilakbas@gmail.com>
2014-01-02 17:41:06 +0200

    Harita Ekranı eklendi.
    GoogleMap ApiKey yaratılıp fragment içinde harita gösterimi yapıldı.
    Ayarlar butonu eklendi.

---
+++ src/com/example/ceptetrafik/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ceptetrafik;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+           
+    }
+
+}
